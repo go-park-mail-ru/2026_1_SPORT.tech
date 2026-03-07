a1e7787b3069f7e2f95615e7897784b02e220a4e
deadlyparkour <deadlyparkour@gmail.com>
2026-03-07 22:56:01 +0300

    added postgres

---
+++ internal/config/config.go
@@ -0,0 +1,47 @@
+package config
+
+import (
+	"fmt"
+	"os"
+)
+
+type Config struct {
+	ServerPort string
+
+	DBHost     string
+	DBPort     string
+	DBUser     string
+	DBPassword string
+	DBName     string
+}
+
+func NewConfig() Config {
+	return Config{
+		ServerPort: getEnv("SERVER_PORT", "8080"),
+
+		DBHost:     getEnv("DB_HOST", "localhost"),
+		DBPort:     getEnv("DB_PORT", "5432"),
+		DBUser:     getEnv("DB_USER", "postgres"),
+		DBPassword: getEnv("DB_PASSWORD", "postgres"),
+		DBName:     getEnv("DB_NAME", "db"),
+	}
+}
+
+func (c *Config) DSN() string {
+	return fmt.Sprintf(
+		"host=%s port=%s user=%s password=%s dbname=%s sslmode=disable",
+		c.DBHost,
+		c.DBPort,
+		c.DBUser,
+		c.DBPassword,
+		c.DBName,
+	)
+}
+
+func getEnv(key, fallback string) string {
+	if value, ok := os.LookupEnv(key); ok {
+		return value
+	}
+
+	return fallback
+}
