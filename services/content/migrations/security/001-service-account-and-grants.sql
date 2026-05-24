09b8bd906498be065ef015d66b2a656340083552
deadlyparkour777 <xwolvax@gmail.com>
2026-05-25 00:12:31 +0300

    .

--- services/content/migrations/security/001-service-account-and-grants.sql
+++ services/content/migrations/security/001-service-account-and-grants.sql
@@ -30,8 +30,8 @@ GRANT SELECT, INSERT ON TABLE content_donation TO :"content_app_user";
 GRANT SELECT, INSERT, UPDATE ON TABLE content_notification TO :"content_app_user";
 GRANT SELECT, INSERT, UPDATE ON TABLE content_payment TO :"content_app_user";
 GRANT SELECT, INSERT, UPDATE ON TABLE content_chat_message TO :"content_app_user";
-GRANT SELECT, INSERT, DELETE ON TABLE content_meeting_availability_rule TO :"content_app_user";
-GRANT SELECT, INSERT, DELETE ON TABLE content_meeting_slot TO :"content_app_user";
+GRANT SELECT, INSERT, UPDATE, DELETE ON TABLE content_meeting_availability_rule TO :"content_app_user";
+GRANT SELECT, INSERT, UPDATE, DELETE ON TABLE content_meeting_slot TO :"content_app_user";
 GRANT SELECT, INSERT, UPDATE ON TABLE content_meeting_booking TO :"content_app_user";
 
 GRANT USAGE, SELECT ON SEQUENCE content_post_post_id_seq TO :"content_app_user";
